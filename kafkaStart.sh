a6131ec501e570b367ce06e5528f4fa35d078f56
daniil <daniilprohorov@gmail.com>
2018-05-10 14:05:24 +0300

    dont use kafkaStart, it doesnt work

--- kafkaStart.sh
+++ kafkaStart.sh
@@ -1,7 +1,4 @@
 #!/usr/bin/bash
 cd kafka_2.11-1.1.0
-echo "lol1"
 ./bin/zookeeper-server-start.sh config/zookeeper.properties > zookeeper &
-sleep 10
 ./bin/kafka-server-start.sh config/server.properties  > server &
-echo "lol2"
